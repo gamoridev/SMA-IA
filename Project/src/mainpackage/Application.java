e2a80471356bc8380b1be829a77fecafe712ce0b
gabrielbreno <gabrielbrenoa@gmail.com>
2016-06-15 17:19:23 -0300

    [ADD] - Main class

---
+++ Project/src/mainpackage/Application.java
@@ -0,0 +1,14 @@
+package mainpackage;
+
+/**
+ *
+ * @author Gabriel Breno Lima Sousa de Amorim - RA:21496990
+ * @author Rafael
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
